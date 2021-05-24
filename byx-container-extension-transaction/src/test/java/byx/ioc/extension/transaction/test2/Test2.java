1dfa9382545aa138c3336eb7a8c38c9ed35600b9
byx2000 <416982860@qq.com>
2021-05-24 10:08:28 +0800

    修改byx-container-extension-transaction的测试代码

--- byx-container-extension-transaction/src/test/java/byx/ioc/extension/transaction/test2/Test2.java
+++ byx-container-extension-transaction/src/test/java/byx/ioc/extension/transaction/test2/Test2.java
@@ -1,19 +1,14 @@
 package byx.ioc.extension.transaction.test2;
 
 import byx.ioc.core.AnnotationConfigContainer;
-import byx.ioc.core.Container;
+import byx.ioc.exception.CannotRegisterInterfaceException;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class Test2 {
     @Test
     public void test() {
-        Container container = new AnnotationConfigContainer(Test2.class);
-        for (String id : container.getObjectIds()) {
-            System.out.println(id);
-        }
-
-        MyService myService = container.getObject(MyService.class);
-        assertNull(myService);
+        assertThrows(CannotRegisterInterfaceException.class, () -> new AnnotationConfigContainer(Test2.class));
     }
 }
