04eacac07ce685429ec2f546fc0bd11123fad336
byx2000 <416982860@qq.com>
2021-06-11 17:01:55 +0800

    添加ValueProcessor

--- byx-container-annotation/src/main/java/byx/ioc/annotation/callback/ComponentProcessor.java
+++ byx-container-annotation/src/main/java/byx/ioc/annotation/callback/ComponentProcessor.java
@@ -1,23 +1,22 @@
 package byx.ioc.annotation.callback;
 
-import byx.ioc.annotation.annotation.*;
+import byx.ioc.annotation.annotation.Autowired;
+import byx.ioc.annotation.annotation.Component;
+import byx.ioc.annotation.annotation.Id;
+import byx.ioc.annotation.annotation.Import;
 import byx.ioc.annotation.core.AnnotationConfigContainerCallback;
 import byx.ioc.annotation.core.AnnotationConfigContainerContext;
 import byx.ioc.annotation.core.ObjectDefinition;
-import byx.ioc.annotation.core.ValueConverter;
 import byx.ioc.annotation.exception.CannotRegisterInterfaceException;
 import byx.ioc.annotation.exception.ConstructorMultiDefException;
 import byx.ioc.annotation.exception.ConstructorNotFoundException;
-import byx.ioc.annotation.exception.ValueConverterNotFoundException;
 import byx.ioc.annotation.util.ClassPredicates;
 import byx.ioc.annotation.util.PackageScanner;
 import byx.ioc.core.Container;
 import byx.ioc.core.Dependency;
 import byx.ioc.util.ExtensionLoader;
 
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Method;
+import java.lang.reflect.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -30,21 +29,16 @@ import java.util.List;
 public class ComponentProcessor implements AnnotationConfigContainerCallback {
     @Override
     public void afterAnnotationConfigContainerInit(AnnotationConfigContainerContext ctx) {
-        // 获取使用SPI机制导入的类
+        // 扫描来源如下;
+        // 1. 包下所有标注了Component注解的类
+        // 2. 所有标注了Component注解的扩展类
+        // 3. 使用Import注解导入的类
         List<Class<?>> classes = new ArrayList<>(ExtensionLoader.getExtensionsWithAnnotation(Component.class));
-
-        // 获取注解扫描器
         PackageScanner scanner = ctx.getPackageScanner();
-
-        // 扫描使用Import注解导入的类
         scanner.getClasses(ClassPredicates.hasAnnotation(Import.class)).stream()
                 .flatMap(c -> Arrays.stream(c.getAnnotation(Import.class).value().clone()))
                 .forEach(classes::add);
-
-        // 扫描指定包下的所有类
         classes.addAll(scanner.getClasses(ClassPredicates.hasAnnotation(Component.class)));
-
-        // 执行注解解析操作
         classes.forEach(c -> processClass(c, ctx));
     }
 
@@ -65,7 +59,7 @@ public class ComponentProcessor implements AnnotationConfigContainerCallback {
         Constructor<?> constructor = getConstructor(type);
 
         // 获取实例化的依赖项
-        Dependency[] dependencies = processDependencies(constructor);
+        Dependency[] dependencies = getParameterDependencies(constructor);
 
         // 创建ObjectDefinition
         ObjectDefinition definition = new ObjectDefinition() {
@@ -91,9 +85,6 @@ public class ComponentProcessor implements AnnotationConfigContainerCallback {
 
         // 注册对象
         ctx.getObjectRegistry().registerObject(id, definition);
-
-        // 处理Value注解
-        processValue(type, ctx);
 
         // 处理方法
         Arrays.stream(type.getMethods())
@@ -130,52 +121,31 @@ public class ComponentProcessor implements AnnotationConfigContainerCallback {
     }
 
     /**
-     * 构造函数依赖项的获取
+     * 获取参数的id
      */
-    private Dependency[] processDependencies(Constructor<?> constructor) {
-        // 获取构造函数参数的注入类型
-        Class<?>[] paramTypes = constructor.getParameterTypes();
-
-        // 获取构造函数参数的注入id
-        Annotation[][] paramAnnotations = constructor.getParameterAnnotations();
-        String[] paramIds = new String[paramTypes.length];
-        for (int i = 0; i < paramTypes.length; ++i) {
-            for (Annotation a : paramAnnotations[i]) {
-                if (a instanceof Id) {
-                    paramIds[i] = ((Id) a).value();
-                }
-            }
+    private String getId(AnnotatedElement element) {
+        if (element.isAnnotationPresent(Id.class)) {
+            return element.getAnnotation(Id.class).value();
         }
+        return null;
+    }
 
-        // 解析依赖项
-        return getDependencies(paramTypes, paramIds);
+    /**
+     * 获取参数依赖项
+     */
+    private Dependency[] getParameterDependencies(Executable executable) {
+        Parameter[] parameters = executable.getParameters();
+        Class<?>[] types = Arrays.stream(parameters).map(Parameter::getType).toArray(Class<?>[]::new);
+        String[] ids = Arrays.stream(parameters).map(this::getId).toArray(String[]::new);
+        return getDependencies(types, ids);
     }
 
     /**
      * 处理被Component标注的方法
      */
     private void processMethod(Class<?> instanceType, Method method, AnnotationConfigContainerContext ctx) {
-        // 获取方法参数注入类型
-        Class<?>[] paramTypes = method.getParameterTypes();
-        Annotation[][] paramAnnotations = method.getParameterAnnotations();
-
-        // 获取方法参数注入id
-        String[] paramIds = new String[paramTypes.length];
-        for (int i = 0; i < paramTypes.length; ++i) {
-            for (Annotation a : paramAnnotations[i]) {
-                if (a instanceof Id) {
-                    paramIds[i] = ((Id) a).value();
-                }
-            }
-        }
-
-        // 获取方法所属的对象实例id
-        String instanceId = instanceType.isAnnotationPresent(Id.class)
-                ? instanceType.getAnnotation(Id.class).value()
-                : null;
-
         // 解析依赖项
-        Dependency[] dependencies = getDependencies(paramTypes, paramIds);
+        Dependency[] dependencies = getParameterDependencies(method);
 
         Container container = ctx.getContainer();
 
@@ -193,9 +163,9 @@ public class ComponentProcessor implements AnnotationConfigContainerCallback {
 
             @Override
             public Object getInstance(Object[] dependencies) {
-                Object instance = (instanceId == null)
-                        ? container.getObject(instanceType)
-                        : container.getObject(instanceId);
+                Object instance = (instanceType.isAnnotationPresent(Id.class))
+                        ? container.getObject(instanceType.getAnnotation(Id.class).value())
+                        : container.getObject(instanceType);
                 try {
                     method.setAccessible(true);
                     return method.invoke(instance, dependencies);
@@ -223,45 +193,5 @@ public class ComponentProcessor implements AnnotationConfigContainerCallback {
             }
         }
         return dependencies;
-    }
-
-    /**
-     * 处理Value注解
-     */
-    private void processValue(Class<?> type, AnnotationConfigContainerContext ctx) {
-        List<Value> values = new ArrayList<>();
-        if (type.isAnnotationPresent(Value.class)) {
-            values.add(type.getAnnotation(Value.class));
-        } else if (type.isAnnotationPresent(Values.class)) {
-            values = Arrays.asList(type.getAnnotation(Values.class).value().clone());
-        }
-
-        for (Value v : values) {
-            ValueConverter converter = getValueConverter(v.type());
-            String id = "".equals(v.id()) ? v.value() : v.id();
-            ctx.getObjectRegistry().registerObject(id, new ObjectDefinition() {
-                @Override
-                public Class<?> getType() {
-                    return v.type();
-                }
-
-                @Override
-                public Object getInstance(Object[] dependencies) {
-                    return converter.convert(v.value());
-                }
-            });
-        }
-    }
-
-    /**
-     * 查找值转换器
-     */
-    private ValueConverter getValueConverter(Class<?> toType) {
-        for (ValueConverter vc : ExtensionLoader.getExtensionInstancesOfType(ValueConverter.class)) {
-            if (vc.fromType() == String.class && vc.toType() == toType) {
-                return vc;
-            }
-        }
-        throw new ValueConverterNotFoundException(String.class, toType);
     }
 }
---
+++ byx-container-annotation/src/main/java/byx/ioc/annotation/callback/ValueProcessor.java
@@ -0,0 +1,55 @@
+package byx.ioc.annotation.callback;
+
+import byx.ioc.annotation.annotation.Value;
+import byx.ioc.annotation.annotation.Values;
+import byx.ioc.annotation.core.AnnotationConfigContainerCallback;
+import byx.ioc.annotation.core.AnnotationConfigContainerContext;
+import byx.ioc.annotation.core.ObjectDefinition;
+import byx.ioc.annotation.core.ValueConverter;
+import byx.ioc.annotation.exception.ValueConverterNotFoundException;
+import byx.ioc.annotation.util.ClassPredicates;
+import byx.ioc.annotation.util.PackageScanner;
+import byx.ioc.util.ExtensionLoader;
+
+import java.util.Arrays;
+
+/**
+ * 处理Value注解
+ *
+ * @author byx
+ */
+public class ValueProcessor implements AnnotationConfigContainerCallback {
+    @Override
+    public void afterAnnotationConfigContainerInit(AnnotationConfigContainerContext ctx) {
+        PackageScanner packageScanner = ctx.getPackageScanner();
+        packageScanner.getClasses(ClassPredicates.hasAnnotation(Value.class))
+                .forEach(c -> processValue(c.getAnnotation(Value.class), ctx));
+        packageScanner.getClasses(ClassPredicates.hasAnnotation(Values.class))
+                .forEach(c -> Arrays.stream(c.getAnnotation(Values.class).value()).forEach(v -> processValue(v, ctx)));
+    }
+
+    private ValueConverter getValueConverter(Class<?> toType) {
+        for (ValueConverter vc : ExtensionLoader.getExtensionInstancesOfType(ValueConverter.class)) {
+            if (vc.fromType() == String.class && vc.toType() == toType) {
+                return vc;
+            }
+        }
+        throw new ValueConverterNotFoundException(String.class, toType);
+    }
+
+    private void processValue(Value v, AnnotationConfigContainerContext ctx) {
+        ValueConverter converter = getValueConverter(v.type());
+        String id = "".equals(v.id()) ? v.value() : v.id();
+        ctx.getObjectRegistry().registerObject(id, new ObjectDefinition() {
+            @Override
+            public Class<?> getType() {
+                return v.type();
+            }
+
+            @Override
+            public Object getInstance(Object[] dependencies) {
+                return converter.convert(v.value());
+            }
+        });
+    }
+}
