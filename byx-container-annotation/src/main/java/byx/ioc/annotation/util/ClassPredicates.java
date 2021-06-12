c1b577e0a054bfc013652405b1f33787a7815679
byx2000 <416982860@qq.com>
2021-06-12 21:19:40 +0800

    完善ClassPredicates

--- byx-container-annotation/src/main/java/byx/ioc/annotation/util/ClassPredicates.java
+++ byx-container-annotation/src/main/java/byx/ioc/annotation/util/ClassPredicates.java
@@ -4,18 +4,36 @@ import java.lang.annotation.Annotation;
 import java.util.function.Predicate;
 
 /**
+ * 预定义的Predicate&lt;Class&lt;?&gt;&gt;
+ *
  * @author byx
  */
 public class ClassPredicates {
+    /**
+     * 匹配所有类
+     */
+    public static Predicate<Class<?>> all() {
+        return c -> true;
+    }
+
+    /**
+     * 匹配标注了指定注解的类
+     */
     public static Predicate<Class<?>> hasAnnotation(Class<? extends Annotation> annotationType) {
         return c -> AnnotationUtils.hasAnnotation(c, annotationType);
     }
 
+    /**
+     * 匹配属于指定子类型的类
+     */
     public static Predicate<Class<?>> isAssignableTo(Class<?> type) {
         return type::isAssignableFrom;
     }
 
-    public static Predicate<Class<?>> equal(Class<?> type) {
+    /**
+     * 匹配特定类型的类
+     */
+    public static Predicate<Class<?>> sameAs(Class<?> type) {
         return c -> c == type;
     }
 }
